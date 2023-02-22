b33583d8bfe7987d1ac9ebc0452ab5efd3ba3ae9
andrew <ajacoboc@unsa.edu.pe>
2023-02-22 16:56:41 -0500

    Bases de la importacion

--- Backend/almacen/integracion-compras/list_compras_detalle.php
+++ Backend/almacen/integracion-compras/list_compras_detalle.php
@@ -3,71 +3,80 @@ include_once "../../common/cors.php";
 include_once "../../common/conexion_integracion.php";
 
 $pdo = getPDO();
-$result = [];
 $message_error = "";
 $description_error = "";
+$result = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $json = file_get_contents('php://input');
-    $data = json_decode($json, true);
 
     if ($pdo) {
-        $sql =
-            "SELECT 
-            c.Cd_Com,
-            c.FecMov,
-            c.Cd_Prv,
-            c.Proveedor,
-            c.Cd_TD,
-            c.NroSre,
-            c.NroDoc,
-            CONVERT(decimal(20,2), c.ValorTotal) AS ValorTotal,
-            CONVERT(decimal(20,2), c.Igv) AS Igv,
-            CONVERT(decimal(20,2), c.Total) AS Total,
-            c.UsuCrea
-            FROM Compra2 c";
-
         try {
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $Cd_Com = $row["Cd_Com"];
-                $row["com2Det"] = [];
 
-                $sql_detalle =
+            // AQUI SE DEBEN DEFINIR 2 ASPECTOS
+            /*
+            - fechas en las que se deben filtrar
+            - estado de la orden de compra
+            */
+
+            $sql_orden_compra =
+                "SELECT 
+                Cd_OC,
+                NroOc AS numero_orden_compra,
+                CAST(FecEmi AS date) AS fecha_emision,
+                CAST(FecEnt AS date) AS fecha_entrega,
+                CAST(FecPag AS date) AS fecha_pago,
+                Cd_Mda AS codigo_moneda,
+                CamMda AS cambio_moneda,
+                Cd_Prv AS codigo_proveedor,
+                Proveedor AS proveedor,
+                ValorTotal AS valor_total,
+                Igv AS igv,
+                Total AS total,
+                C_IGV_TASA AS igv_tasa,
+                C_IB_CERRADO_ATENCION_PARCIAL AS atencion_parcial,
+                C_PENDIENTE_CANCELADO AS pendiente
+                 FROM OrdCompra2
+            WHERE CAST(FecEmi AS date) >= '2023-02-21' AND CAST(FecEmi AS date) <= '2023-02-21'";
+
+            $stmt_orden_compra = $pdo->prepare($sql_orden_compra);
+            $stmt_orden_compra->execute();
+
+            while ($row = $stmt_orden_compra->fetch(PDO::FETCH_ASSOC)) {
+                $row["ordComDet"] = [];
+                $Cd_Oc = $row["Cd_OC"];
+
+                $sql_orden_compra_detalle =
                     "SELECT
-                cd.Cd_Com,
-                cd.Item,
-                cd.Cd_Prod,
-                cd.Descrip,
-                CONVERT(decimal(20,2), cd.BimUni) AS BimUni,
-                CONVERT(decimal(20,2), cd.IgvUni) AS IgvUni,
-                CONVERT(decimal(20,2), cd.PrecioUni) AS PrecioUni,
-                CONVERT(decimal(20,2), cd.Cant) AS Cant,
-                CONVERT(decimal(20,2), cd.ValorTotal) AS ValorTotal,
-                CONVERT(decimal(20,2), cd.Bim) AS Bim,
-                CONVERT(decimal(20,2), cd.Igv) AS Igv,
-                CONVERT(decimal(20,2), cd.Total) AS Total,
-                cd.FecEnt
-                FROM CompraDet2 cd
-                WHERE cd.Cd_Com = ?
-                ";
-                $stmt_detalle = $pdo->prepare($sql_detalle);
-                $stmt_detalle->bindParam(1, $Cd_Com, PDO::PARAM_STR);
-                $stmt_detalle->execute();
-                while ($row_detalle = $stmt_detalle->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($row["com2Det"], $row_detalle);
+                    Cd_OC,
+                    Item AS item,
+                    Cd_Prod AS codigo_producto,
+                    Descrip AS descripcion,
+                    ValorUni AS valor_unitario,
+                    IgvUni AS igv_unitario,
+                    PrecioUni AS precio_unidad,
+                    CONVERT(decimal(20,2), Cant) AS cantidad,
+                    ValorTotal AS valor_total,
+                    Igv AS igv,
+                    Total AS total,
+                    CAST(FecEnt AS date) AS fecha_entrega
+                    FROM OrdCompraDet2 WHERE Cd_OC = ?";
+                $stmt_orden_compra_detalle = $pdo->prepare($sql_orden_compra_detalle);
+                $stmt_orden_compra_detalle->bindParam(1, $Cd_Oc, PDO::PARAM_STR);
+
+                $stmt_orden_compra_detalle->execute();
+                while ($row_detalle = $stmt_orden_compra_detalle->fetch(PDO::FETCH_ASSOC)) {
+                    array_push($row["ordComDet"], $row_detalle);
                 }
                 array_push($result, $row);
             }
         } catch (PDOException $e) {
+            $message_error = "ERROR INTERNO SERVER";
+            $description_error = $e->getMessage();
         }
     } else {
         $message_error = "Error con la conexion a la base de datos";
         $description_error = "Error con la conexion a la base de datos a traves de PDO";
     }
-
-    // Retornamos el resultado
     $return['message_error'] = $message_error;
     $return['description_error'] = $description_error;
     $return['result'] = $result;
